5349ab32298e23f63632e9f1913775d9efb65bd9
goonie77 <goonie77@f9e4c086-e717-0410-a497-f4739b3d3a8a>
2008-07-08 22:13:01 +0000

    Added loan rules 1-5, 10
    git-svn-id: https://fpml-toolkit.svn.sourceforge.net/svnroot/fpml-toolkit/Java/trunk@323 f9e4c086-e717-0410-a497-f4739b3d3a8a

--- src-handcoded/com/handcoded/fpml/validation/LoanRules.java
+++ src-handcoded/com/handcoded/fpml/validation/LoanRules.java
@@ -23,6 +23,8 @@ import com.handcoded.xml.DOM;
 import com.handcoded.xml.NodeIndex;
 import com.handcoded.xml.XPath;
 
+import java.math.BigDecimal;
+
 /**
  * The <CODE>LoanRules</CODE> class contains a <CODE>RuleSet</CODE>
  * initialised with FpML defined validation rules for syndicated loan messages.
@@ -103,9 +105,8 @@ public final class LoanRules extends FpMLRuleSet
 			for (int index = 0; index < list.getLength(); ++index) {
 				Element		context = (Element) list.item (index);
 				Element		effective	= XPath.path (context, "effectiveDate");
-				Element		rateIndex	= XPath.path (context, "currentInterestRatePeriod", "floatingRateIndex");
 				Element		fixingDate	= XPath.path (context, "currentInterestRatePeriod", "rateFixingDate");
-				
+				Element		rateIndex	= XPath.path (context, "currentInterestRatePeriod", "floatingRateIndex");
 				
 				if ((fixingDate != null) && (effective != null) && (DOM.getInnerText (rateIndex).contains("PRIME"))) {
 					if (notEqual (toDate (fixingDate) , toDate (effective))){
@@ -178,6 +179,144 @@ public final class LoanRules extends FpMLRuleSet
 	};
 	
 	/**
+	 * A <CODE>Rule</CODE> that ensures that if mandatoryCostRate doesn't exist and interestRate and margin and allInRate exist, 
+	 * then allInRate = margin + interestRate 
+	 * <P>
+	 * Applies to FpML 4.4 and later.
+	 * @since	TFP 1.2
+	 */
+	public static final Rule RULE04 = new Rule (Preconditions.R4_4__LATER, "ln-4") {
+		
+		public boolean validate (NodeIndex nodeIndex, ValidationErrorHandler errorHandler)
+		{
+			if (nodeIndex.hasTypeInformation()) 
+				return (
+					  validate (nodeIndex.getElementsByType (determineNamespace (nodeIndex), "InterestRatePeriod"), errorHandler));					
+				
+			return (
+				  validate (nodeIndex.getElementsByName ("currentInterestRatePeriod"), errorHandler));
+		}
+		
+		private boolean validate (NodeList list, ValidationErrorHandler errorHandler)
+		{
+			boolean		result	= true;
+			
+			for (int index = 0; index < list.getLength(); ++index) {
+				Element		context = (Element) list.item (index);
+				Element		costRate	= XPath.path (context, "mandatoryCostRate");
+				Element		interestRate	= XPath.path (context, "interestRate");
+				Element		margin	= XPath.path (context,"margin");
+				Element		allInRate	= XPath.path (context,"allInRate");
+			
+				if ((costRate == null) && (interestRate != null) && (margin != null) && (allInRate != null)){
+					BigDecimal allInRateValue = toDecimal (allInRate);
+					BigDecimal marginValue = toDecimal (margin);
+					BigDecimal interestRateValue = toDecimal (interestRate);
+					BigDecimal marginPlusInterest = marginValue.add(interestRateValue);
+					
+					if (allInRateValue.compareTo(marginPlusInterest) != 0)
+						errorHandler.error ("305", context,
+								"The allInRate must be equal to margin + interestRate",
+								getName (), null);
+						result = false;
+				}						
+			}
+			return (result);
+		}
+		
+	};
+	
+	/**
+	 * A <CODE>Rule</CODE> that ensures that if mandatoryCostRate and interestRate and margin and allInRate exist, 
+	 * then allInRate = margin + interestRate + mandatoryCostRate
+	 * <P>
+	 * Applies to FpML 4.4 and later.
+	 * @since	TFP 1.2
+	 */
+	public static final Rule RULE05 = new Rule (Preconditions.R4_4__LATER, "ln-5") {
+		
+		public boolean validate (NodeIndex nodeIndex, ValidationErrorHandler errorHandler)
+		{
+			if (nodeIndex.hasTypeInformation()) 
+				return (
+					  validate (nodeIndex.getElementsByType (determineNamespace (nodeIndex), "InterestRatePeriod"), errorHandler));					
+				
+			return (
+				  validate (nodeIndex.getElementsByName ("currentInterestRatePeriod"), errorHandler));
+		}
+		
+		private boolean validate (NodeList list, ValidationErrorHandler errorHandler)
+		{
+			boolean		result	= true;
+			
+			for (int index = 0; index < list.getLength(); ++index) {
+				Element		context = (Element) list.item (index);
+				Element		costRate	= XPath.path (context, "mandatoryCostRate");
+				Element		interestRate	= XPath.path (context, "interestRate");
+				Element		margin	= XPath.path (context,"margin");
+				Element		allInRate	= XPath.path (context,"allInRate");
+			
+				if ((costRate!= null) && (interestRate != null) && (margin != null) && (allInRate !=null)){
+					
+					BigDecimal allInRateValue = toDecimal (allInRate);
+					BigDecimal marginValue = toDecimal (margin);
+					BigDecimal interestRateValue = toDecimal (interestRate);
+					BigDecimal costRateValue = toDecimal (costRate);
+					BigDecimal marginPlusInterest = marginValue.add(interestRateValue);
+					BigDecimal totalMarginPlusCost = marginPlusInterest.add(costRateValue);
+							
+					if (allInRateValue.compareTo(totalMarginPlusCost) != 0)
+						errorHandler.error ("305", context,
+							"The allInRate must be equal to margin + interestRate + mandatoryCostRate",
+							getName (), null);
+							result = false;
+				}						
+			}
+			return (result);
+		}
+		
+	};
+	
+	public static final Rule RULE10 = new Rule (Preconditions.R4_4__LATER, "ln-10") {
+		
+		public boolean validate (NodeIndex nodeIndex, ValidationErrorHandler errorHandler)
+		{
+			if (nodeIndex.hasTypeInformation()) 
+				return (
+					  validate (nodeIndex.getElementsByType (determineNamespace (nodeIndex), "FacilityNotice"), errorHandler));					
+				
+			return (
+				  validate (nodeIndex.getElementsByName ("facilityNotice"), errorHandler));
+		}
+		
+		private boolean validate (NodeList list, ValidationErrorHandler errorHandler)
+		{
+			boolean		result	= true;
+			
+			for (int index = 0; index < list.getLength(); ++index) {
+				Element		context = (Element) list.item (index);
+				Element		facilityAmount	= XPath.path (context, "facilityIdentifier", "originalCommitmentAmount");
+				Element		loanAmount	= XPath.path (context, "facilityCommitmentPosition", "loanContractPosition", "loanContractIdentifier", "originalAmount");
+				
+				if ((facilityAmount != null) && (loanAmount != null) && (isSameCurrency(facilityAmount,loanAmount))){
+					Element originalCommitment = XPath.path(facilityAmount, "amount");
+					Element originalAmount = XPath.path(loanAmount, "amount");
+					
+					if (less(toDecimal(originalCommitment), toDecimal(originalAmount))){
+						errorHandler.error ("305", context,
+								"The facilityIdentifier/originalCommitmentAmount/amount must be greater than or equal to the facilityCommitmentPosition/loanContractPosition/loanContractIdentifier/originalAmount/amount",
+								getName (), null);		
+						result = false;
+					}
+					
+				}
+			}
+			return (result);
+		}
+	};
+	
+	
+	/**
 	 * Provides access to the Loan validation rule set.
 	 *
 	 * @return	The Loan validation rule set.
@@ -189,20 +328,31 @@ public final class LoanRules extends FpMLRuleSet
 	}
 
 	/**
-	 * A <CODE>RuleSet</CODE> containing all the standard FpML defined
-	 * validation rules for loan messages.
+	 * Ensures no instances can be created.
 	 * @since	TFP 1.2
 	 */
-	private static final RuleSet	rules = new RuleSet ();
+	private LoanRules ()
+	{ }
+
+	/**
+	 * A <CODE>RuleSet</CODE> containing all the standard FpML defined
+	 * validation rules for syndicated loans.
+	 * @since	TFP 1.2
+	 */
+	private static final RuleSet	rules = RuleSet.forName ("LoanRules");
 	
 	/**
-	 * Initialises the <CODE>RuleSet</CODe> by adding the individually defined
-	 * validation rules.
-	 * @since	TFP 1.2
+	 * Determine if two <CODE>Element</CODE> structures containing
+	 * <B>Money</B> instances have the same currency code.
+	 * 
+	 * @param	moneyA			The <CODE>Element</CODE> containing the first <B>Money</B>.
+	 * @param	moneyB			The <CODE>Element</CODE> containing the second <B>Money</B>.
+	 * @return	<CODE>true</CODE> if both <B>Money</B> structures have the same currency.
+	 * @since	TFP 1.1
 	 */
-	static {
-		rules.add (RULE01);
-		rules.add (RULE02);
-		rules.add (RULE03);
+	private static boolean isSameCurrency (Element moneyA, Element moneyB)
+	{
+		return (equal (XPath.path (moneyA, "currency"),
+					   XPath.path (moneyB, "currency")));
 	}
 }
